2561532dc6e2df1eb5ccdbbfa50270cbf2e2bb4d
Éric Lemoine <eric.lemoine@camptocamp.com>
2011-10-05 13:51:15 +0000

    add a loader script, that can autoload others
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/cgxp@1442 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

---
+++ core/src/script/CGXP/loader.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2011 Camptocamp
+ *
+ * CGXP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CGXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CGXP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function() {
+
+    var jsfiles = new Array(
+        "plugins/ThemeSelector.js",
+        "locale/en.js",
+        "locale/fr.js",
+        "locale/de.js",
+        "locale/it.js"
+    );
+    
+    var scripts = document.getElementsByTagName("script");
+    var parts = scripts[scripts.length-1].src.split("/");
+    parts.pop();
+    var path = parts.join("/");
+
+    var len = jsfiles.length;
+    var pieces = new Array(len);
+
+    for (var i=0; i<len; i++) {
+        pieces[i] = "<script src='" + path + "/" + jsfiles[i] + "'></script>"; 
+    }
+    document.write(pieces.join(""));
+    
+    if (GeoExt.Lang) {
+        GeoExt.Lang.set(OpenLayers.Util.getParameters()["lang"] || GeoExt.Lang.locale);
+    }
+
+})();
+
