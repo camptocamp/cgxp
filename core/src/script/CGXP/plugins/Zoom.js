9b2d6ad40e86d342b13b3b976c0caa72dde74af5
Alex Saunier <alexandre.saunier@camptocamp.com>
2011-10-10 12:18:23 +0000

    added Zoom plugin in CGXP
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/cgxp@1458 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

---
+++ core/src/script/CGXP/plugins/Zoom.js
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2011 Camptocamp
+ *
+ * CGXP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CGXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CGXP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @requires plugins/Zoom.js
+ * @include GeoExt/widgets/Action.js
+ */
+
+/** api: (define)
+ *  module = cgxp.plugins
+ *  class = Zoom
+ */
+
+/** api: (extends)
+ *  plugins/Zoom.js
+ */
+Ext.namespace("cgxp.plugins");
+
+/** api: constructor
+ *  .. class:: Zoom(config)
+ *
+ *    Provides two actions for zooming in and out.
+ */
+cgxp.plugins.Zoom = Ext.extend(gxp.plugins.Zoom, {
+
+    /** api: ptype = cgxp_zoom */
+    ptype: "cgxp_zoom",
+
+    /** api: method[addActions]
+     */
+    addActions: function() {
+        var actions = [new GeoExt.Action({
+            menuText: this.zoomInMenuText,
+            iconCls: "gxp-icon-zoom-in",
+            tooltip: this.zoomInTooltip,
+            control: new OpenLayers.Control.ZoomBox(),
+            map: this.target.mapPanel.map,
+            enableToggle: true,
+            toggleGroup: this.toggleGroup,
+            allowDepress: true
+        }), {
+            menuText: this.zoomOutMenuText,
+            iconCls: "gxp-icon-zoom-out",
+            tooltip: this.zoomOutTooltip,
+            handler: function() {
+                this.target.mapPanel.map.zoomOut();
+            },
+            scope: this
+        }];
+        return gxp.plugins.Zoom.superclass.addActions.apply(this, [actions]);
+    }
+
+});
+
+Ext.preg(cgxp.plugins.Zoom.prototype.ptype, cgxp.plugins.Zoom);
