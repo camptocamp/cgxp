9a0994ecdcc15623c5180ea536e3f66071282a6f
Alex Saunier <alexandre.saunier@camptocamp.com>
2011-10-18 13:47:52 +0000

    added help button plugin r=sbrunner
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/cgxp@1513 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

---
+++ core/src/script/CGXP/plugins/Help.js
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2011 Camptocamp
+ *
+ * CGXP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CGXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CGXP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @requires plugins/Tool.js
+ */
+
+/** api: (define)
+ *  module = cgxp.plugins
+ *  class = Help
+ */
+
+/** api: (extends)
+ *  plugins/Tool.js
+ */
+Ext.namespace("cgxp.plugins");
+
+/** api: constructor
+ *  .. class:: Help(config)
+ *
+ *    Provides an "help" button.
+ */
+cgxp.plugins.Help = Ext.extend(gxp.plugins.Tool, {
+
+    /** api: ptype = cgxp_help */
+    ptype: "cgxp_help",
+
+    /** api: config[url]
+     *  URL of help page to open.
+     */
+    url: null,
+
+    /** api: method[addActions]
+     */
+    addActions: function() {
+        var action = new GeoExt.Action({
+            iconCls: "help",
+            tooltip: OpenLayers.i18n("Tools.helpactiontooltip"),
+            handler: function() {
+                window.open(this.url);
+            },
+            scope: this
+        });
+        return cgxp.plugins.Help.superclass.addActions.apply(this, [action]);
+    }
+});
+
+Ext.preg(cgxp.plugins.Help.prototype.ptype, cgxp.plugins.Help);
