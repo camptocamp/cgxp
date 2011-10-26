9417bff064fd42cf7661031a898553d782d0c36c
Alex Saunier <alexandre.saunier@camptocamp.com>
2011-10-26 09:46:52 +0000

    added EPSG:2056 definition
    git-svn-id: https://project.camptocamp.com/svn/c2c_mapfish/cgxp@1584 42eb0e99-3e3d-0410-99bf-a43aee6ce3a3

---
+++ proj4js/EPSG2056.js
@@ -0,0 +1,8 @@
+/*
+ * @requires proj4js/lib/proj4js.js
+ * @include proj4js/lib/projCode/somerc.js
+ * @include proj4js/lib/projCode/merc.js
+ */
+
+Proj4js.defs["EPSG:2056"] = "+title=CH1903 / LV95 +proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs";
+
