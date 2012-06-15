0b2c2cebce4e47ed5f7d8558fdb52cb95099c19f
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2012-06-15 14:09:32 +0200

    first version

---
+++ core/src/script/CGXP/plugins/WFSPermalink.js
@@ -0,0 +1,174 @@
+/**
+ * Copyright (c) 2012 Camptocamp
+ *
+ * CGXP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CGXP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CGXP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * @requires plugins/Tool.js
+ * @include OpenLayers/Control/GetFeature.js
+ * @include OpenLayers/Protocol/WFS/v1_0_0.js
+ * @include OpenLayers/Format/GML.js
+ * @include OpenLayers/Filter/Comparison.js
+ * @include OpenLayers/Filter/Logical.js
+ */
+
+
+/** api: (define)
+ *  module = cgxp.plugins
+ *  class = WFSPermalink
+ */
+
+Ext.namespace("cgxp.plugins");
+
+/** api: constructor
+ *  .. class:: WFSPermalink(config)
+ *
+ *    This plugin is used to perform a WFS request using permalink parameters.
+ */
+cgxp.plugins.WFSPermalink = Ext.extend(gxp.plugins.Tool, {
+
+    /** api: ptype = cgxp_wfspermalink */
+    ptype: "cgxp_wfspermalink",
+
+    componentConfig: null,
+
+    init: function() {
+        cgxp.plugins.WFSPermalink.superclass.init.apply(this, arguments);
+        this.componentConfig.target = this.target;
+        new cgxp.WFSPermalink(this.componentConfig);
+    }
+});
+
+Ext.preg(cgxp.plugins.WFSPermalink.prototype.ptype,
+         cgxp.plugins.WFSPermalink);
+
+/** api: (define)
+ *  module = cgxp
+ *  class = WFSPermalink
+ */
+
+Ext.namespace("cgxp");
+
+/** api: constructor
+ *  .. class:: WFSPermalink(config)
+ *
+ *    Utility class that parses the URL parameters and make a WFS request.
+ */
+cgxp.WFSPermalink = Ext.extend(Ext.Component, {
+
+    stateId: 'search',
+
+    WFSTypes: null,
+    WFSURL: null,
+    geometryName: 'geom',
+    maxFeatures: 100,
+    events: null,
+    target: null,
+    srsName: null,
+
+    layername: null,
+    filters: null,
+
+    initComponent: function() {
+        cgxp.WFSPermalink.superclass.initComponent.apply(this, arguments);
+
+        if (!this.WFSTypes && this.WFSGetFeatureId &&
+            this.target.tools[this.WFSGetFeatureId]) {
+            this.WFSTypes = this.target.tools[this.WFSGetFeatureId].WFSTypes;
+        }
+    },
+    
+    applyState: function(state) {
+        if (!state.layer || this.WFSTypes.indexOf(state.layer) == -1) {
+            // layername is missing or unknown => do nothing
+            return;
+        }
+        this.layername = state.layer;
+        delete state.layer;
+        this.filters = state;
+
+        var protocol = new OpenLayers.Protocol.WFS({
+            url: this.WFSURL,
+            geometryName: this.geometryName,
+            featureType: this.layername,
+            srsName: this.srsName || this.target.mapPanel.map.getProjection(),
+            formatOptions: {
+                featureNS: 'http://mapserver.gis.umn.edu/mapserver',
+                autoconfig: false
+            }
+        });
+
+        this.events.fireEvent('querystarts');
+        protocol.read({
+            filter: this.createFilter(),
+            maxFeatures: this.maxFeatures,
+            callback: this.handleResult,
+            scope: this
+        });
+    },
+
+    createFilter: function() {
+        if (!this.filters) {
+            return null;
+        }
+        
+        var filters = [], prop;
+        for (prop in this.filters) {
+            if (!this.filters[prop]) {
+                continue;
+            }
+            filters.push(new OpenLayers.Filter.Comparison({
+                type: OpenLayers.Filter.Comparison.EQUAL_TO,
+                property: prop,
+                value: this.filters[prop]
+            }));
+        }
+        
+        if (!filters) {
+            return null;
+        }
+
+        return new OpenLayers.Filter.Logical({
+            type: OpenLayers.Filter.Logical.AND,
+            filters: filters
+        });
+    },
+
+    handleResult: function(result) {
+        if (result.success() && result.features.length) {
+            var features = result.features;
+            console.log(features)
+            if (!(OpenLayers.Util.isArray(features))) {
+                features = [features];
+            }
+
+            var geometry = null, maxExtent = null;
+            for(var i=0, len=features.length; i<len; i++) {
+                geometry = features[i].geometry;
+                if (geometry) {
+                    if (maxExtent === null) {
+                        maxExtent = new OpenLayers.Bounds();
+                    }
+                    maxExtent.extend(geometry.getBounds());
+                }
+            }
+            if (maxExtent) {
+                this.target.mapPanel.map.zoomToExtent(maxExtent);
+            }
+
+            this.events.fireEvent('queryresults', features);
+        }
+    }
+});
