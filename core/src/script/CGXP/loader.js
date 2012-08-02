828ba1ec9b7b372cb0c0681760df56a73704c332
pgiraud <pierre.giraud@camptocamp.com>
2012-08-02 17:14:07 +0200

    Removing trailing spaces

--- core/src/script/CGXP/loader.js
+++ core/src/script/CGXP/loader.js
@@ -54,7 +54,7 @@
         "locale/de.js",
         "locale/it.js"
     ];
-    
+
     var scripts = document.getElementsByTagName("script");
     var parts = scripts[scripts.length-1].src.split("/");
     parts.pop();
@@ -64,10 +64,10 @@
     var pieces = new Array(len);
 
     for (var i=0; i<len; i++) {
-        pieces[i] = "<script src='" + path + "/" + jsfiles[i] + "'></script>"; 
+        pieces[i] = "<script src='" + path + "/" + jsfiles[i] + "'></script>";
     }
     document.write(pieces.join(""));
-    
+
     if (GeoExt.Lang) {
         GeoExt.Lang.set(OpenLayers.Util.getParameters()["lang"] || GeoExt.Lang.locale);
     }
