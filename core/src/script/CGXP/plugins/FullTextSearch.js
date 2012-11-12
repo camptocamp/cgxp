0a358283efeb81c0361a5e84445848d3ca820343
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2012-11-12 10:30:32 +0100

    Adding API doc to tell about GroupComboBox dependency

--- core/src/script/CGXP/plugins/FullTextSearch.js
+++ core/src/script/CGXP/plugins/FullTextSearch.js
@@ -145,6 +145,8 @@ cgxp.plugins.FullTextSearch = Ext.extend(gxp.plugins.Tool, {
      *  ``Boolean``
      *  Tells whether to group the results by ``layer_name``. If set to ``true``,
      *  the data returned by the service is intended to include such a field.
+     *  If set to ``true``, ``GroupComboBox.js`` needs to be included as part
+     *  of the built file if any.
      *  Defaults to ``false``.
      */
     grouping: false,
