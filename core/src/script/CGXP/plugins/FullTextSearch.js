93d760341456070c18bb0efaa7f1b80746b55c36
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2012-11-12 09:39:27 +0100

    Grouping now defaults to true

--- core/src/script/CGXP/plugins/FullTextSearch.js
+++ core/src/script/CGXP/plugins/FullTextSearch.js
@@ -145,9 +145,9 @@ cgxp.plugins.FullTextSearch = Ext.extend(gxp.plugins.Tool, {
      *  ``Boolean``
      *  Tells whether to group the results by ``layer_name``. If set to true,
      *  the data returned by the service is intended to include such a field.
-     *  Defaults to ``false``.
+     *  Defaults to ``true``.
      */
-    grouping: false,
+    grouping: true,
 
     init: function() {
         cgxp.plugins.FullTextSearch.superclass.init.apply(this, arguments);
