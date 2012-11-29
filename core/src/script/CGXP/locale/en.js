94ecc1fe0a16cf5c71a1a2218773944685e38363
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2012-11-29 10:23:09 +0100

    Missing translations

--- core/src/script/CGXP/locale/en.js
+++ core/src/script/CGXP/locale/en.js
@@ -76,6 +76,7 @@ GeoExt.Lang.add("en", {
         scalefieldText: "Scale",
         rotationfieldText: "Rotation",
         printbuttonText: "Print",
+        printbuttonTooltip: "Print",
         exportpngbuttonText: "Export in PNG",
         waitingText: "Printing...",
         downloadText: 'Download',
