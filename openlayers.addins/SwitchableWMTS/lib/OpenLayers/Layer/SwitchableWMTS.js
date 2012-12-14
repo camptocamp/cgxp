55f960e5b70e1442ed7f2898b3d9f34f55ed31f8
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2012-12-14 20:59:16 +0100

    add doc for SwitchableWMTS

--- openlayers.addins/SwitchableWMTS/lib/OpenLayers/Layer/SwitchableWMTS.js
+++ openlayers.addins/SwitchableWMTS/lib/OpenLayers/Layer/SwitchableWMTS.js
@@ -1,7 +1,22 @@
+/* Copyright (c) 2006-2012 by OpenLayers Contributors (see authors.txt for 
+ * full list of contributors). Published under the 2-clause BSD license.
+ * See license.txt in the OpenLayers distribution or repository for the
+ * full text of the license. */
+
 /*
  * @requires OpenLayers/Layer/WMTS.js
  */
 
+/**
+ * Class: OpenLayers.Layer.SwitchableWMTS
+ * Extend the standard <OpenLayers.Layer.WMTS> class, adding support of two
+ *     new configuration parameters that make the service base URL switch to
+ *     the ``secondaryUrl`` when the current zoomlevel is greater than
+ *     ``zoomThreshold``.
+ * 
+ * Inherits from:
+ *  - <OpenLayers.Layer.WMTS>
+ */
 OpenLayers.Layer.SwitchableWMTS = OpenLayers.Class(OpenLayers.Layer.WMTS, {
 
     /** 
