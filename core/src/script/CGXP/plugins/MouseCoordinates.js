ddbdc7bb67b419632d66f1eb65c304164a586c67
Alexandre Saunier <alexandre.saunier@camptocamp.com>
2012-12-18 17:31:39 +0100

    Add plugin displaying the mouse cursor ccoordinates in a toolbar

---
+++ core/src/script/CGXP/plugins/MouseCoordinates.js
@@ -0,0 +1,104 @@
+/**
+* Copyright (c) 2012 SITN, Canton et République de Neuchâtel, Camptocamp
+*
+* CGXP is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* CGXP is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with CGXP. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @include OpenLayers/Control/MousePosition.js
+ */
+
+/** api: (define)
+ *  module = cgxp.plugins
+ *  class = MouseCoordinates
+ */
+
+/** api: (extends)
+ *  plugins/Tool.js
+ */
+Ext.namespace("cgxp");
+
+/** api: example
+ *  Sample code showing how to add a MouseCoordinates plugin to a
+ *  `gxp.Viewer`:
+ *
+ *  .. code-block:: javascript
+ *
+ *      new gxp.Viewer({
+ *          ...
+ *          tools: [{
+ *              ptype: 'cgxp_mousecoordinates',
+ *              actionTarget: 'center.bbar',
+ *              controlConfig: {
+ *                  numDigits: 0,
+ *                  prefix: 'Lon: ',
+ *                  separator: ', lat: ',
+ *                  displayProjection: "EPSG:4326"
+ *              }
+ *          }]
+ *          ...
+ *      });
+ */
+
+/** api: constructor
+ *  .. class:: MouseCoordinates(config)
+ *
+ *  Provides an action to show map coordinates in an Ext toolbar.
+ */
+cgxp.MouseCoordinates = Ext.extend(gxp.plugins.Tool, {
+
+    /** api: ptype = cgxp_mousecoordinates */
+    ptype: "cgxp_mousecoordinates",
+
+    /** api: config[separator]
+     *  ``String``
+     *  Text shown between the coordinates (i18n).
+     */
+    separator: ", ",
+
+    /** api: config[numDigits]
+     *  ``Integer``
+     *  Number of digits shown after the decimal mark.
+     */
+    numDigits: 0,
+
+    /** api: config[controlConfig]
+     *  ``Object``
+     *  Optional parameter containing an <OpenLayers.Control.MousePosition>
+     *  config.
+     */
+    controlConfig: null,
+
+    /** api: method[addActions]
+     */
+    addActions: function() {
+        var statustext = new Ext.Toolbar.TextItem({
+            cls: 'mouse-coordinates',
+            listeners: {
+                afterRender: function(cmp) {
+                    var control = new OpenLayers.Control.MousePosition(Ext.apply({
+                        div: cmp.getEl().dom,
+                        numDigits: this.numDigits,
+                        separator: this.separator
+                    }, this.controlConfig));
+                    this.target.mapPanel.map.addControl(control);
+                },
+                scope: this
+            }
+        });
+        return cgxp.MouseCoordinates.superclass.addActions.apply(this, [statustext]);
+    }
+});
+
+Ext.preg(cgxp.MouseCoordinates.prototype.ptype, cgxp.MouseCoordinates);
