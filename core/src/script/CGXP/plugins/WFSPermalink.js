269fcb15d5af7c32b138751f0feb1680824574da
Alex Saunier <alexandre.saunier@camptocamp.com>
2013-02-20 18:45:07 +0100

    Add support of groups of filters in WFSPermalink

--- core/src/script/CGXP/plugins/WFSPermalink.js
+++ core/src/script/CGXP/plugins/WFSPermalink.js
@@ -181,7 +181,7 @@ cgxp.plugins.WFSPermalink = Ext.extend(gxp.plugins.Tool, {
 
         this.events.fireEvent('querystarts');
         protocol.read({
-            filter: this.createFilter(layerName, state),
+            filter: this.createFilter(state),
             maxFeatures: this.maxFeatures,
             callback: function(response) {
                 this.handleResponse(response, layerName);
@@ -192,14 +192,55 @@ cgxp.plugins.WFSPermalink = Ext.extend(gxp.plugins.Tool, {
 
     /** private: method[createFilter]
      *  Build a WFS filter according to the permalink parameters.
-     *  :param layerName: ``String`` The layer name.
      *  :param state: ``Object`` The state object.
      */
-    createFilter: function(layerName, state) {
+    createFilter: function(state) {
         if (!state) {
             return null;
         }
 
+        if (typeof state.ngroups != "undefined") {
+            var nb = parseInt(state.ngroups);
+            if (typeof nb != "number") {
+                return null;
+            }
+            delete state.ngroups;
+            var filterGroups = [], filter, params, prop, prefix;
+            for (var i = 0; i < nb; i++) {
+                prefix = String(i) + "_";
+                params = {};
+                // look for state properties starting with the current prefix
+                for (prop in state) {
+                    if (prop.indexOf(prefix) == 0) {
+                        params[prop.substring(prefix.length)] = state[prop];
+                        delete state[prop];
+                    }
+                }
+                filter = this.createGroupFilter(params);
+                if (filter) {
+                    filterGroups.push(filter);
+                }
+            }
+            if (!filterGroups) {
+                return null;
+            }
+            if (filterGroups.length == 1) {
+                return filterGroups[0];
+            }
+            return new OpenLayers.Filter.Logical({
+                type: OpenLayers.Filter.Logical.OR,
+                filters: filterGroups
+            });
+        }
+
+        return this.createGroupFilter(state);
+    },
+
+    /** private: method[createGroupFilter]
+     *  Build a WFS filter according to a group of permalink parameters.
+     *  :param state: ``Object`` The full state object or a subset (group).
+     */
+    createGroupFilter: function(state) {
         var filters = [], prop, values, propFilters, i, len;
         for (prop in state) {
             if (!state[prop]) {
