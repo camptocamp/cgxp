1120ac147a15ac348a6e90c0c0673023c7b0abf7
Éric Lemoine <eric.lemoine@gmail.com>
2013-04-18 15:18:55 +0200

    Make WMTS overlays queryable with WMS GFI

--- core/src/script/CGXP/widgets/tree/LayerTree.js
+++ core/src/script/CGXP/widgets/tree/LayerTree.js
@@ -851,8 +851,8 @@ cgxp.tree.LayerTree = Ext.extend(Ext.tree.TreePanel, {
                                 dimension: child.dimension,
                                 visibility: child.isChecked,
                                 isBaseLayer: false,
-                                mapserverURL: child.mapserverURL,
-                                mapserverLayers: child.mapserverLayers
+                                mapserverURL: child.wmsUrl,
+                                mapserverLayers: child.wmsLayers
                             }, this.wmtsOptions || {}));
                             child.node.attributes.layer = layer;
                             if (this.initialState && this.initialState['opacity_' + child.name]) {
