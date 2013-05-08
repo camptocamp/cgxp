4cbb6cbed0d27040d2394e7936b860469f401de1
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-05-08 14:12:02 +0000

    Migrate documentation server to wheezy

--- core/src/doc/update_online.sh
+++ core/src/doc/update_online.sh
@@ -12,8 +12,8 @@
 #
 # Possible Apache config:
 #
-# Alias /cgxp "/var/www/docs.camptocamp.net/htdocs/c2cgeoportal/html"
-# <Directory "/var/www/docs.camptocamp.net/htdocs/cgxp/html">
+# Alias /cgxp "/var/www/vhosts/docs.camptocamp.net/htdocs/c2cgeoportal/html"
+# <Directory "/var/www/vhosts/docs.camptocamp.net/htdocs/cgxp/html">
 #     Options Indexes FollowSymLinks MultiViews
 #     AllowOverride None
 #     Order allow,deny
@@ -22,7 +22,7 @@
 #
 
 # BUILDDIR is where the HTML files are generated
-BUILDDIR=/var/www/docs.camptocamp.net/htdocs/cgxp
+BUILDDIR=/var/www/vhosts/docs.camptocamp.net/htdocs/cgxp
 
 # create the build dir if it doesn't exist
 if [[ ! -d ${BUILDDIR} ]]; then
@@ -34,7 +34,7 @@ git pull origin master
 
 # create a virtual env if none exists already
 if [[ ! -d env ]]; then
-    curl http://mapfish.org/downloads/virtualenv-1.4.5.py | python - --no-site-packages --distribute env
+    virtualenv --no-site-packages --distribute env
 fi
 
 # install JSTools, Jinga2 and Sphinx
