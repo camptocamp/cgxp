38e90c68f0a2a31613b4d4567ef2459a10158142
Paul Spencer <paul@pagameba.com>
2013-09-26 09:39:51 -0400

    Refactor FullTextSearch into a plugin and a widget.

--- core/src/script/CGXP/plugins/FullTextSearch.js
+++ core/src/script/CGXP/plugins/FullTextSearch.js
@@ -57,25 +57,11 @@ cgxp.plugins.FullTextSearch = Ext.extend(gxp.plugins.Tool, {
     /** api: ptype = cgxp_fulltextsearch */
     ptype: "cgxp_fulltextsearch",
 
-    /** api: config[tooltipTitle]
-     *  ``String`` Text for the tooltip title (i18n). Only applies if
-     *  :attr:`tooltip` is ``true``.
+    /** api: config[widgetOptions]
+     *  ``Object``
+     *  Additional FullTextSearch widget options.
      */
-    tooltipTitle: "Search",
-    /** api: config[emptyText]
-     *  ``String`` Text to use when the field is empty (i18n).
-     */
-    emptyText: "Search...",
-    /** api: config[loadingText]
-     *  ``String`` Text to display while loading results (i18n).
-     */
-    loadingText: "Searching...",
-
-    /** api: config[tooltip]
-     *  ``Boolean`` Whether to display a tooltip above the search box.
-     *  Default is ``true``.
-     */
-    tooltip: true,
+    widgetOptions: {},
 
     /** api: config[url]
      *  ``String`` URL of the text search service. Typically set to
@@ -114,12 +100,6 @@ cgxp.plugins.FullTextSearch = Ext.extend(gxp.plugins.Tool, {
      */
     coordsRecenteringStyle: null,
 
-    /** api: config[comboWidth]
-     *  ``Number``
-     *  Width in pixels of the search combobox.
-     */
-    comboWidth: 200,
-
     /** api: config[actionConfig]
      *  ``Object``
      *  Optional configuration of the ComboBox.
@@ -140,47 +120,6 @@ cgxp.plugins.FullTextSearch = Ext.extend(gxp.plugins.Tool, {
      *  Optional configuration of the vector layer.
      */
     vectorLayerConfig: {},
-
-    /** api: config[grouping]
-     *  ``Boolean``
-     *  Tells whether to group the results by ``layer_name``. If set to ``true``,
-     *  the data returned by the service is intended to include such a field.
-     *  If set to ``true``, ``GroupComboBox.js`` needs to be included as part
-     *  of the built file if any.
-     *  Defaults to ``false``.
-     */
-    grouping: false,
-
-    /** api: config[limits]
-     *  ``Object`` Option object to configure search
-     *  limit parameters sent to the text search
-     *  service. The possible properties are:
-     *
-     *  * ``limit`` - ``Number`` The maximum number of
-     *    results in the response.
-     *  * ``partitionlimit`` - ``Number`` The maximum number
-     *    of results per layer/group in the response.
-     *
-     *  ``partitionlimit`` is typically used when the ``grouping``
-     *  option is to ``true``, to limit the number of
-     *  results in each group.
-     *
-     *  If the ``limits`` option is unspecified the limit
-     *  parameters sent in search requests depend whether
-     *  ``grouping`` is ``true`` or ``false``:
-     *
-     *  * If ``grouping`` is ``false`` then ``limit`` is set to ``20``,
-     *    and ``partitionlimit`` is not set.
-     *  * If ``grouping`` is ``true`` then ``limit`` is set to ``40``,
-     *    and ``partitionlimit`` is set to ``10``.
-     *
-     *  Any provided ``limits`` object is *applied* to the
-     *  default values. For example, if ``grouping`` is
-     *  ``true`` and if the ``limits`` option is set to
-     *  ``{limit: 50}`` then ``limit`` will be set to ``50`` and
-     *  ``partitionlimit`` will be set to ``10`` in search requests.
-     */
-    limits: {},
 
     init: function() {
         cgxp.plugins.FullTextSearch.superclass.init.apply(this, arguments);
@@ -222,66 +161,8 @@ cgxp.plugins.FullTextSearch = Ext.extend(gxp.plugins.Tool, {
     /** private: method[addActions]
      */
     addActions: function() {
-        var combo = this.createCombo();
+        this.fullTextSearch = this.createCombo();
         return cgxp.plugins.FullTextSearch.superclass.addActions.apply(this, [combo]);
-    },
-
-    createStore: function() {
-        var baseParams = Ext.apply(this.grouping ?
-                { limit: 40, partitionlimit: 10 } :
-                { limit: 20 }, this.limits);
-        var store = new GeoExt.data.FeatureStore({
-            proxy: new Ext.data.ScriptTagProxy({
-                url: this.url,
-                callbackParam: 'callback'
-            }),
-            baseParams: baseParams,
-            reader: new cgxp.data.FeatureReader({
-                format: new OpenLayers.Format.GeoJSON()
-            }, ['label', 'layer_name']),
-            sortInfo: this.grouping ? {field: 'layer_name', direction: 'ASC'} : null
-        });
-
-        store.on('beforeload', function(store, options) {
-            var coords = store.baseParams.query.match(
-                /([\d\.']+)[\s,]+([\d\.']+)/
-            );
-            this.position = null;
-            this.closeLoading.cancel();
-            this.applyPositionTask.cancel();
-            if (coords) {
-                var map = this.target.mapPanel.map;
-                var left = parseFloat(coords[1].replace("'", ""));
-                var right = parseFloat(coords[2].replace("'", ""));
-
-                var tryProjection = function(lon, lat, projection) {
-                    var position = new OpenLayers.LonLat(lon, lat);
-                    position.transform(projection, map.getProjectionObject());
-                    if (map.maxExtent.containsLonLat(position)) {
-                        this.position = position;
-                        return true;
-                    }
-                    return false;
-                }.createDelegate(this);
-
-                for (var projection in this.projections) {
-                    if (tryProjection(left, right, projection) ||
-                        tryProjection(right, left, projection)) {
-                        break;
-                    }
-                }
-
-                if (this.position) {
-                    // close the loading twin box.
-                    this.closeLoading.delay(10);
-                    // apply the position
-                    this.applyPositionTask.delay(1000);
-                    return false;
-                }
-            }
-            return true;
-        }, this);
-        return store;
     },
 
     /** private: method[applyPosition]
@@ -304,40 +185,59 @@ cgxp.plugins.FullTextSearch = Ext.extend(gxp.plugins.Tool, {
 
     /** private: method[createCombo]
      *
-     *  :returns ``Ext.form.ComboBox`` The search combo.
+     *  :returns ``cgxp.FullTextSearch`` The search widget.
      */
     createCombo: function() {
         var map = this.target.mapPanel.map;
-        var tpl = new Ext.XTemplate(
-            '<tpl for="."><div class="x-combo-list-item">',
-            '{label}',
-            '</div></tpl>'
-        );
-        var comboClass = this.grouping ?
-            Ext.ux.form.GroupComboBox : Ext.ux.form.TwinTriggerComboBox;
-        var combo = new comboClass(Ext.apply({
-            store: this.createStore(),
-            tpl: tpl,
-            minChars: 1,
-            queryDelay: 50,
-            emptyText: this.emptyText,
-            loadingText: this.loadingText,
-            displayField: 'label',
-            triggerAction: 'all',
-            trigger2Class: 'x-form-trigger-no-width x-hidden',
-            trigger3Class: 'x-form-trigger-no-width x-hidden',
-            width: this.comboWidth,
-            selectOnFocus: true
-        }, this.actionConfig));
-        // used to close the loading panel
-        this.closeLoading = new Ext.util.DelayedTask(function () {
-            combo.list.hide();
-        }, this);
+        var combo = cgxp.FullTextSearch(Ext.apply({
+            url: this.url
+        }, this.widgetOptions));
+
         // used to apply the position
         this.applyPositionTask = new Ext.util.DelayedTask(function () {
             this.applyPosition();
         }, this);
+
         combo.on({
+            'beforeload': function(store, options) {
+                var coords = store.baseParams.query.match(
+                    /([\d\.']+)[\s,]+([\d\.']+)/
+                );
+                this.position = null;
+                this.closeLoading.cancel();
+                this.applyPositionTask.cancel();
+                if (coords) {
+                    var map = this.target.mapPanel.map;
+                    var left = parseFloat(coords[1].replace("'", ""));
+                    var right = parseFloat(coords[2].replace("'", ""));
+
+                    var tryProjection = function(lon, lat, projection) {
+                        var position = new OpenLayers.LonLat(lon, lat);
+                        position.transform(projection, map.getProjectionObject());
+                        if (map.maxExtent.containsLonLat(position)) {
+                            this.position = position;
+                            return true;
+                        }
+                        return false;
+                    }.createDelegate(this);
+
+                    for (var projection in this.projections) {
+                        if (tryProjection(left, right, projection) ||
+                            tryProjection(right, left, projection)) {
+                            break;
+                        }
+                    }
+
+                    if (this.position) {
+                        // close the loading twin box.
+                        this.closeLoading.delay(10);
+                        // apply the position
+                        this.applyPositionTask.delay(1000);
+                        return false;
+                    }
+                }
+                return true;
+            },
             'select': function(combo, record, index) {
                 // add feature to vector layer
                 var feature = record.getFeature();
@@ -371,27 +271,6 @@ cgxp.plugins.FullTextSearch = Ext.extend(gxp.plugins.Tool, {
             },
             'clear': function(combo) {
                 this.vectorLayer.removeFeatures(this.vectorLayer.features);
-            },
-            'render': function(component) {
-                if (this.tooltip) {
-                    new Ext.ToolTip({
-                        target: combo.getEl(),
-                        title: this.tooltipTitle,
-                        width: 500,
-                        contentEl: 'search-tip',
-                        trackMouse: true,
-                        dismissDelay: 15000
-                    });
-                }
-                function stop(e) {
-                    var event = e || window.event;
-                    if (event.stopPropagation) {
-                        event.stopPropagation();
-                    } else {
-                        event.cancelBubble = true;
-                    }
-                }
-                component.getEl().dom.onkeydown = stop;
             },
             'specialkey': function(field, event) {
                 if (this.position && event.getKey() == event.ENTER) {
