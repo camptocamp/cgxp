04b1bcb64cc373164c17eeae3f3e624a76d515d9
Stéphane Brunner <stephane.brunner@camptocamp.com>
2013-11-28 16:13:46 +0100

    Use 4 spaces indentation

--- openlayers.addins/AddViaPoint/lib/OpenLayers/Control/AddViaPoint.js
+++ openlayers.addins/AddViaPoint/lib/OpenLayers/Control/AddViaPoint.js
@@ -25,155 +25,155 @@
 
 OpenLayers.Control.AddViaPoint = OpenLayers.Class(OpenLayers.Control, {
 
-  vertexStyleConfig: {
-    graphicName: 'circle',
-    pointRadius: 5,
-    strokeOpacity: 1,
-    strokeWidth: 1,
-    strokeColor: '#000000',
-    fillOpacity: 1,
-    fillColor: '#ffffff'
-  },
+    vertexStyleConfig: {
+        graphicName: 'circle',
+        pointRadius: 5,
+        strokeOpacity: 1,
+        strokeWidth: 1,
+        strokeColor: '#000000',
+        fillOpacity: 1,
+        fillColor: '#ffffff'
+    },
 
-  geometryTypes: 'OpenLayers.Geometry.LineString',
+    geometryTypes: 'OpenLayers.Geometry.LineString',
 
-  tolerance: 5,
+    tolerance: 5,
 
-  EVENT_TYPES: ['startviapoint','moveviapoint','endviapoint'],
+    EVENT_TYPES: ['startviapoint', 'moveviapoint', 'endviapoint'],
 
-  initialize: function(layer, options) {
-    OpenLayers.Control.prototype.initialize.apply(this, [options]);
-    this.layer = layer;
-    this.handler = new OpenLayers.Handler.Hover(this,{
-      move: this.onMouseMove
-    });
+    initialize: function(layer, options) {
+        OpenLayers.Control.prototype.initialize.apply(this, [options]);
+        this.layer = layer;
+        this.handler = new OpenLayers.Handler.Hover(this, {
+            move: this.onMouseMove
+        });
 
-    this.dragHandler = new OpenLayers.Handler.Drag(this, {
-      down: this.dragDown,
-      move: this.dragMove,
-      done: this.dragDone
-    });
-    this.vertexStyle = OpenLayers.Util.applyDefaults(this.vertexStyleConfig, OpenLayers.Feature.Vector.style['default']);
-  },
+        this.dragHandler = new OpenLayers.Handler.Drag(this, {
+            down: this.dragDown,
+            move: this.dragMove,
+            done: this.dragDone
+        });
+        this.vertexStyle = OpenLayers.Util.applyDefaults(this.vertexStyleConfig, OpenLayers.Feature.Vector.style['default']);
+    },
 
-  dragDown: function dragDown(pixel) {
-    var feature = this.layer.getFeatureFromEvent(this.dragHandler.evt);
-    if (feature && feature == this.virtualVertex) {
-      this.events.triggerEvent('startviapoint', {feature: this.virtualVertex});
-      this.handler.deactivate();
-      this.isDragging = true;
-    }
-  },
-
-  dragMove: function dragMove(pixel) {
-    if (this.isDragging) {
-      var pos = this.map.getLonLatFromViewPortPx(pixel);
-      var geom = this.virtualVertex.geometry;
-      geom.move(pos.lon-geom.x,pos.lat-geom.y);
-      this.layer.drawFeature(this.virtualVertex);
-      this.events.triggerEvent('moveviapoint', {feature: this.virtualVertex});
-    }
-  },
-
-  dragDone: function dragDone() {
-    if (this.isDragging) {
-      this.isDragging = false;
-      this.events.triggerEvent('endviapoint', {feature: this.virtualVertex});
-      this.virtualVertex = null;
-      this.handler.activate();
-    }
-  },
-
-  onMouseMove: function(evt) {
-    var lonLat = this.map.getLonLatFromViewPortPx(evt.xy);
-    var mousePoint = new OpenLayers.Geometry.Point(lonLat.lon, lonLat.lat);
-    var dist = this.tolerance * this.map.resolution;
-    var n = this.layer.features.length;
-    var closestPoint;
-    var result;
-    var i;
-    var feature;
-
-    for (i=0;i<n;i++) {
-      feature = this.layer.features[i];
-      if (this.geometryTypeMatches(feature)) {
-        closestPoint = this.findClosestOnFeature(mousePoint, feature, dist);
-        if (closestPoint) {
-          if (this.virtualVertex) {
-            this.virtualVertex.geometry.x = closestPoint.x;
-            this.virtualVertex.geometry.y = closestPoint.y;
-            if (!this.virtualVertex.layer) {
-              this.layer.addFeatures([this.virtualVertex],{silent:true});
-            }
-            this.layer.drawFeature(this.virtualVertex);
-          } else {
-            this.virtualVertex = new OpenLayers.Feature.Vector(
-              new OpenLayers.Geometry.Point(closestPoint.x,closestPoint.y),
-              {},
-              OpenLayers.Util.extend({},this.vertexStyle)
-            );
-            this.layer.addFeatures([this.virtualVertex], {silent:true});
-          }
-          this.dragHandler.setMap(this.layer.map);
-          this.dragHandler.activate();
-          break;
+    dragDown: function dragDown(pixel) {
+        var feature = this.layer.getFeatureFromEvent(this.dragHandler.evt);
+        if (feature && feature == this.virtualVertex) {
+            this.events.triggerEvent('startviapoint', {feature: this.virtualVertex});
+            this.handler.deactivate();
+            this.isDragging = true;
         }
-      }
-      feature = null;
-    }
-    if (!feature && this.virtualVertex) {
-      this.layer.removeFeatures([this.virtualVertex], {silent: true});
-      this.dragHandler.deactivate();
-    }
-  },
+    },
 
-  geometryTypeMatches: function(feature) {
-      return this.geometryTypes == null ||
-          OpenLayers.Util.indexOf(this.geometryTypes,
-                                  feature.geometry.CLASS_NAME) > -1;
-  },
+    dragMove: function dragMove(pixel) {
+        if (this.isDragging) {
+            var pos = this.map.getLonLatFromViewPortPx(pixel);
+            var geom = this.virtualVertex.geometry;
+            geom.move(pos.lon-geom.x, pos.lat-geom.y);
+            this.layer.drawFeature(this.virtualVertex);
+            this.events.triggerEvent('moveviapoint', {feature: this.virtualVertex});
+        }
+    },
 
-  getSegments: function(geometry) {
-    var components = geometry.components;
-    var numSeg = components.length - 1;
-    var segments = [];
-    var point1
-    var point2;
-    var i;
+    dragDone: function dragDone() {
+        if (this.isDragging) {
+            this.isDragging = false;
+            this.events.triggerEvent('endviapoint', {feature: this.virtualVertex});
+            this.virtualVertex = null;
+            this.handler.activate();
+        }
+    },
 
-    for(i=0; i<numSeg; ++i) {
-        point1 = components[i];
-        point2 = components[i + 1];
-        segments[i] = {
-            x1: point1.x,
-            y1: point1.y,
-            x2: point2.x,
-            y2: point2.y
-        };
-    }
-    return segments;
-  },
+    onMouseMove: function(evt) {
+        var lonLat = this.map.getLonLatFromViewPortPx(evt.xy);
+        var mousePoint = new OpenLayers.Geometry.Point(lonLat.lon, lonLat.lat);
+        var dist = this.tolerance * this.map.resolution;
+        var n = this.layer.features.length;
+        var closestPoint;
+        var result;
+        var i;
+        var feature;
 
-  findClosestOnFeature: function(pt, feature, tolerance) {
-    var result = null;
-    var segs = this.getSegments(feature.geometry);
-    var i;
-    var minDistance = tolerance;
-    var dist;
+        for (i=0;i<n;i++) {
+            feature = this.layer.features[i];
+            if (this.geometryTypeMatches(feature)) {
+                closestPoint = this.findClosestOnFeature(mousePoint, feature, dist);
+                if (closestPoint) {
+                    if (this.virtualVertex) {
+                        this.virtualVertex.geometry.x = closestPoint.x;
+                        this.virtualVertex.geometry.y = closestPoint.y;
+                        if (!this.virtualVertex.layer) {
+                            this.layer.addFeatures([this.virtualVertex],{silent:true});
+                        }
+                        this.layer.drawFeature(this.virtualVertex);
+                    } else {
+                        this.virtualVertex = new OpenLayers.Feature.Vector(
+                            new OpenLayers.Geometry.Point(closestPoint.x, closestPoint.y),
+                            {},
+                            OpenLayers.Util.extend({}, this.vertexStyle)
+                        );
+                        this.layer.addFeatures([this.virtualVertex], {silent:true});
+                    }
+                    this.dragHandler.setMap(this.layer.map);
+                    this.dragHandler.activate();
+                    break;
+                }
+                feature = null;
+            }
+        }
+        if (!feature && this.virtualVertex) {
+            this.layer.removeFeatures([this.virtualVertex], {silent: true});
+            this.dragHandler.deactivate();
+        }
+    },
 
-    for (i=0; i<segs.length; i++) {
-      dist = OpenLayers.Geometry.distanceToSegment(pt, segs[i]);
-      if (dist.distance < minDistance) {
-        result = {
-          x: dist.x,
-          y: dist.y
-        };
-        minDistance = dist.distance;
-      }
-    }
-    return result;
-  },
+    geometryTypeMatches: function(feature) {
+            return this.geometryTypes == null ||
+                OpenLayers.Util.indexOf(this.geometryTypes,
+                    feature.geometry.CLASS_NAME) > -1;
+    },
 
-  CLASS_NAME: "OpenLayers.Control.AddViaPoint"
+    getSegments: function(geometry) {
+        var components = geometry.components;
+        var numSeg = components.length - 1;
+        var segments = [];
+        var point1
+        var point2;
+        var i;
+
+        for(i=0; i<numSeg; ++i) {
+                point1 = components[i];
+                point2 = components[i + 1];
+                segments[i] = {
+                        x1: point1.x,
+                        y1: point1.y,
+                        x2: point2.x,
+                        y2: point2.y
+                };
+        }
+        return segments;
+    },
+
+    findClosestOnFeature: function(pt, feature, tolerance) {
+        var result = null;
+        var segs = this.getSegments(feature.geometry);
+        var i;
+        var minDistance = tolerance;
+        var dist;
+
+        for (i=0; i<segs.length; i++) {
+            dist = OpenLayers.Geometry.distanceToSegment(pt, segs[i]);
+            if (dist.distance < minDistance) {
+                result = {
+                    x: dist.x,
+                    y: dist.y
+                };
+                minDistance = dist.distance;
+            }
+        }
+        return result;
+    },
+
+    CLASS_NAME: "OpenLayers.Control.AddViaPoint"
 
 });
