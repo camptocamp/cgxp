5440ce3f9a228bd52b8aa829223377e11a8e8845
Stéphane Brunner <stephane.brunner@camptocamp.com>
2014-02-17 16:18:40 +0100

    Standardize documentation

--- core/src/script/CGXP/plugins/WFSPermalink.js
+++ core/src/script/CGXP/plugins/WFSPermalink.js
@@ -97,7 +97,8 @@ cgxp.plugins.WFSPermalink = Ext.extend(gxp.plugins.Tool, {
 
     /** api: config[stateId]
      *  ``String``
-     * Prefix of the permalink parameters. Default is "wfs". (Optional)
+     * Prefix of the permalink parameters. Default is "wfs".
+     * Optional.
      */
     stateId: 'wfs',
 
@@ -105,7 +106,8 @@ cgxp.plugins.WFSPermalink = Ext.extend(gxp.plugins.Tool, {
      *  ``Array``
      *  The queryable type on the internal server. Can be obtained
      *  from any ``cgxp.plugins.WFSGetFeature`` tool configured in
-     *  the viewer. See the ``WFSGetFeatureId`` option. (Optional)
+     *  the viewer. See the ``WFSGetFeatureId`` option.
+     *  Optional.
      */
     WFSTypes: null,
 
@@ -119,32 +121,37 @@ cgxp.plugins.WFSPermalink = Ext.extend(gxp.plugins.Tool, {
 
     /** api: config[WFSURL]
      *  ``String``
-     *  The mapserver proxy URL. (Required)
+     *  The mapserver proxy URL.
+     *  Required.
      */
     WFSURL: null,
 
     /** api: config[maxFeatures]
      *  ``Number``
-     *  Maximum number of features returned. Default is 100. (Optional)
+     *  Maximum number of features returned.
+     *  Default is 100.
      */
     maxFeatures: 100,
 
     /** api: config[events]
      *  ``Object``
-     *  An Observer used to send events. (Required)
+     *  An Observer used to send events.
+     *  Required.
      */
     events: null,
 
     /** api: config[srsName]
      *  ``String``
-     *  Projection code. (Required)
+     *  Projection code.
+     *  Required.
      */
     srsName: null,
 
     /** api: config[pointRecenterZoom]
      *  ``Number``
      *  Zoom level to use when result is a single point feature. If
-     *  not set the map is not zoomed to a specific zoom level. (Optional)
+     *  not set the map is not zoomed to a specific zoom level.
+     *  Optional.
      */
     pointRecenterZoom: null,
 
