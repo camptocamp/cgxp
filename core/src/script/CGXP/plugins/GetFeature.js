589dc777c8356a672a2ada5bae4e89b162ada21b
Stéphane Brunner <stephane.brunner@camptocamp.com>
2014-08-27 16:39:43 +0200

    Remove Backward compatibility patch

--- core/src/script/CGXP/plugins/GetFeature.js
+++ core/src/script/CGXP/plugins/GetFeature.js
@@ -17,11 +17,11 @@
 
 /*
  * @requires plugins/Tool.js
- * @include CGXP/patches/GML.js
  * @include OpenLayers/Control/GetFeature.js
  * @include OpenLayers/Control/WMSGetFeatureInfo.js
  * @include OpenLayers/Protocol/WFS/v1_1_0.js
  * @include OpenLayers/Format/WMSGetFeatureInfo.js
+ * @include OpenLayers/Format/GML/v3.js
  * @include GeoExt/widgets/Action.js
  * @include CGXP/plugins/ToolActivateMgr.js
  */
